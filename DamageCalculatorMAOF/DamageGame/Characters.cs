4b575dfdf9adc4cb23916ac256015df3b678a2ae
Rayne <rayne.r.binneman@gmail.com>
2025-02-04 07:50:33 +0200

    Created a Character Class with a template for any characters that I might make, commented out all the old stuff

--- DamageCalculatorMAOF/DamageGame/Characters.cs
+++ DamageCalculatorMAOF/DamageGame/Characters.cs
@@ -8,56 +8,123 @@
 
 public class Characters
 {
-    public static float hp;
-    public static float armour;
-    public static float characterWeight;
-    public static float strength;
-    public static float agility;
-    public static float moveSpeed = (5f * agility) / characterWeight;
-    public static float characterForce;
-    public static float weaponWeight;
-    public static PhysicalDamageTypes weaponType;
-    public static string weaponName;
+    public float health;
+    public float armour;
+    public float characterWeight;
+    public float strength;
+    public float agility;
+    public float weaponWeight;
+    public PhysicalDamageTypes weaponType;
+    public string weaponName;
+    public string characterName;
 
-    public Characters(float hp, float armour, float characterWeight, float strength, float agility, float weaponWeight, PhysicalDamageTypes weaponType, string weaponName)
+    public Characters(float hp, float ar, float cw, float st, float ag, float ww, PhysicalDamageTypes wt, string wn,
+        string ch)
     {
-        hp = Characters.hp;
-        armour = Characters.armour;
-        characterWeight = Characters.characterWeight;
-        strength = Characters.strength;
-        agility = Characters.agility;
-        weaponWeight = Characters.weaponWeight;
-        weaponType = Characters.weaponType;
-        weaponName = Characters.weaponName;
+        health = hp;
+        armour = ar;
+        characterWeight = cw;
+        strength = st;
+        agility = ag;
+        weaponWeight = ww;
+        weaponType = wt;
+        weaponName = wn;
+        characterName = ch;
     }
-}
 
-public class Gulemark
-{
-    public static float hp = 320f;
-    public static float armour = 40f;
-    public static float characterWeight = 60f;
-    public static float strength = 18f;
-    public static float agility = 2f;
-    public static float moveSpeed = (5f * agility) / characterWeight;
-    public static float characterForce = characterWeight * strength;
-    public static float weaponWeight = 15f;
-    public static PhysicalDamageTypes physType = PhysicalDamageTypes.Blunt;
-    public static string weaponName = "The Tenderiser";
-}
+    public float GetTrueDamage()
+    {
+        float damage = DamageCalculator.TrueDamageCalculator(strength, weaponWeight, weaponType);
+        return damage;
+    }
 
-public class Raikin
-{
-    public static float hp = 80;
-    public static float armour = 30f;
-    public static float characterWeight = 30f;
-    public static float higherAgility = 12f;
-    public static float LowerAgility = 6f;
-    public static float moveSpeed = (5f * higherAgility) / characterWeight;
-    public static float lowerStrength = 12f;
-    public static float higherStrength = 18f;
-    public static float characterForce = characterWeight * higherStrength;
-    public static float weaponWeight = 13f;
-    public static PhysicalDamageTypes spoonLight = PhysicalDamageTypes.Slash;
-    public static PhysicalDamageTypes spoonHeavy = PhysicalDamageTypes.Chop;
+    public float GetAttackSpeed()
+    {
+        float atkSpeed = DamageCalculator.AttackSpeedCalculator(strength, weaponWeight, agility);
+        return atkSpeed;
+    }
+
+    public float GetDps()
+    {
+        float dps = DamageCalculator.DpsCalculator(GetAttackSpeed(), GetTrueDamage());
+        return dps;
+    }
+
+    public float GetMoveSpeed()
+    {
+        float moveSpeed = (5f * agility) / characterWeight;
+        return moveSpeed;
+    }
+
+    public float GetCharacterForce()
+    {
+        float characterForce = characterWeight * strength;
+        return characterForce;
+    }
+
+    public float GetArDamageMod()
+    {
+        float damageMod = DamageModifier.ApDamageModifier(weaponType);
+        return damageMod;
+    }
+
+    public float GetHpDamageMod()
+    {
+        float damageMod = DamageModifier.HpDamageModifier(weaponType);
+        return damageMod;
+    }
+
+    public void GetCharacterDetails()
+    {
+        Console.WriteLine($"Character Name: {characterName}\n");
+
+        Console.WriteLine($"Starting Health Points: {health}");
+        Console.WriteLine($"Starting Armour Points: {armour}");
+        Console.WriteLine($"Character Weight:       {characterWeight}\n");
+
+        Console.WriteLine($"Weapon Name:            {weaponName}");
+        Console.WriteLine($"Weapon Weight:          {weaponWeight}");
+        Console.WriteLine($"Strength:               {strength}");
+        Console.WriteLine($"Weapon Damage Type:     {weaponType.ToString()}");
+        Console.WriteLine($"Calculated Damage:      {GetTrueDamage()}\n");
+
+        Console.WriteLine($"Agility:                {agility}");
+        Console.WriteLine($"Movement Speed:         {GetMoveSpeed()}");
+        Console.WriteLine($"Attack Speed:           {GetAttackSpeed()}");
+        Console.WriteLine($"Damage Per Second:      {GetDps()}\n");
+
+        Console.WriteLine($"Armour Damage Modifier: {GetArDamageMod()}");
+        Console.WriteLine($"Health Damage Modifier: {GetHpDamageMod()}\n");
+        Console.WriteLine("-----------------------------------");
+    }
+
+    // public class Gulemark
+    // {
+    //     public static float hp = 320f;
+    //     public static float armour = 40f;
+    //     public static float characterWeight = 60f;
+    //     public static float strength = 18f;
+    //     public static float agility = 2f;
+    //     public static float moveSpeed = (5f * agility) / characterWeight;
+    //     public static float characterForce = characterWeight * strength;
+    //     public static float weaponWeight = 15f;
+    //     public static PhysicalDamageTypes physType = PhysicalDamageTypes.Blunt;
+    //     public static string weaponName = "The Tenderiser";
+    // }
+    //
+    // public class Raikin
+    // {
+    //     public static float hp = 80;
+    //     public static float armour = 30f;
+    //     public static float characterWeight = 30f;
+    //     public static float higherAgility = 12f;
+    //     public static float LowerAgility = 6f;
+    //     public static float moveSpeed = (5f * higherAgility) / characterWeight;
+    //     public static float lowerStrength = 12f;
+    //     public static float higherStrength = 18f;
+    //     public static float characterForce = characterWeight * higherStrength;
+    //     public static float weaponWeight = 13f;
+    //     public static PhysicalDamageTypes spoonLight = PhysicalDamageTypes.Slash;
+    //     public static PhysicalDamageTypes spoonHeavy = PhysicalDamageTypes.Chop;
+    // }
 }
--- DamageCalculatorMAOF/Program.cs
+++ DamageCalculatorMAOF/Program.cs
@@ -1,96 +1,104 @@
 ﻿using DamageCalculatorMAOF.DamageGame;
 
-Characters gulemark = new Characters(320f, 40f, 60f, 18f, 5f, 15f, PhysicalDamageTypes.Blunt, "The Tenderiser");
+Characters gulemark = new Characters(320f, 40f, 60f, 18f, 5f, 15f, PhysicalDamageTypes.Blunt, "The Tenderiser", "Gulemark");
+Characters raikin = new Characters(80f, 30f, 30f, 18f, 12f, 13f, PhysicalDamageTypes.Chop, "War Spoon", "Raikin");
 
 string userInputString = string.Empty;
 
-//attacking character
-float weaponWeight = Raikin.weaponWeight;
-float strength = Raikin.higherStrength;
-PhysicalDamageTypes weaponDamageType = Raikin.spoonHeavy;
-float agilityValue = Raikin.LowerAgility;
-float characterWeight = Raikin.characterWeight;
-float characterForce = Raikin.characterForce;
+//Character 1
+gulemark.GetCharacterDetails();
+
+//Character 2
+raikin.GetCharacterDetails();
 
 
-//targeted character
-float heatlhValue = Gulemark.hp;
-float armourValue = Gulemark.armour;
-
-while (heatlhValue > 0)
-{
-    float armourDamageModifier = DamageModifier.ApDamageModifier(weaponDamageType);
-    float healthDamageModifier = DamageModifier.HpDamageModifier(weaponDamageType);
-    float damage = DamageCalculator.TrueDamageCalculator(strength, weaponWeight);
-    float newArmourValue = DamageCalculator.ApDamage(damage, armourValue, armourDamageModifier);
-    float remainingDamageAfterArmour = DamageCalculator.CalculatedRemainingDamage(damage, armourValue, armourDamageModifier);
-    float newHealthValue =
-        DamageCalculator.HpDamage(remainingDamageAfterArmour, heatlhValue, newArmourValue, healthDamageModifier);
-    float remainingDamageAfterHealth = remainingDamageAfterArmour - heatlhValue;
-    float attackSpeed = DamageCalculator.AttackSpeedCalculator(strength, weaponWeight, agilityValue);
-    float damagePerSecond = DamageCalculator.DpsCalculator(attackSpeed, damage);
-    
-    Console.WriteLine($"Targeted Character Starting Health Points:  {heatlhValue}");
-    Console.WriteLine($"Targeted Character Starting Armour Points:  {armourValue}");
-    Console.WriteLine("");
-    Console.WriteLine($"Attacking Character Weapon Weight:          {weaponWeight}");
-    Console.WriteLine($"Attacking Character Strength:               {strength}");
-    Console.WriteLine($"Attacking Character Weapon Damage Type:     {weaponDamageType.ToString()}");
-    Console.WriteLine($"Attacking Character Calculated Damage:      {damage}");
-    Console.WriteLine($"Attacking Character Agility:                {agilityValue}");
-    Console.WriteLine($"Attacking Character Attack Speed:           {attackSpeed}");
-    Console.WriteLine($"Attacking Character Damage Per Second:      {damagePerSecond}");
-    Console.WriteLine("");
-    Console.WriteLine($"Attacking Character Armour Damage Modifier: {armourDamageModifier}");
-    Console.WriteLine($"Attacking Character Health Damage Modifier: {healthDamageModifier}");
-    Console.WriteLine("");
-    
-    if (remainingDamageAfterArmour != damage)
-    {
-        Console.WriteLine($"Damage Remaining After Armour:              {remainingDamageAfterArmour}");
-    }
-    else
-    {
-        Console.WriteLine($"Damage did not breach armour");
-    }
-    
-    if (newHealthValue > 0)
-    {
-        Console.WriteLine("");
-        Console.WriteLine($"Targeted Character Armour Points After Damage: {newArmourValue}");
-        Console.WriteLine($"Targeted Character Health Points After Damage: {newHealthValue}");
-        while (userInputString != "Exit")
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Would you like to deal damage again? (y/n)");
-            userInputString = Console.ReadLine();
-
-            if (userInputString == "y")
-            {
-                heatlhValue = newHealthValue;
-                armourValue = newArmourValue;
-                userInputString = "Exit";
-            }
-            else if (userInputString == "n")
-            {
-                heatlhValue = 0;
-                userInputString = "Exit";
-            }
-            else
-            {
-                Console.WriteLine("Invalid Input");
-            }
-        }
-        userInputString = string.Empty;
-    }
-    else
-    {
-        heatlhValue = 0;
-        Console.WriteLine($"Targeted Character Armour Points After Damage:  {newArmourValue}");
-        Console.WriteLine($"Targeted Character Health Points After Damage:  {newHealthValue}");
-        Console.WriteLine($"Remaining Damage:                               {remainingDamageAfterHealth}");
-        Console.WriteLine("");
-    }
-    
-}
-Console.WriteLine("Damage Calculator Stopped");
+// //attacking character
+// float weaponWeight = Raikin.weaponWeight;
+// float strength = Raikin.higherStrength;
+// PhysicalDamageTypes weaponDamageType = Raikin.spoonHeavy;
+// float agilityValue = Raikin.LowerAgility;
+// float characterWeight = Raikin.characterWeight;
+// float characterForce = Raikin.characterForce;
+//
+//
+// //targeted character
+// float heatlhValue = Gulemark.hp;
+// float armourValue = Gulemark.armour;
+//
+// while (heatlhValue > 0)
+// {
+//     float armourDamageModifier = DamageModifier.ApDamageModifier(weaponDamageType);
+//     float healthDamageModifier = DamageModifier.HpDamageModifier(weaponDamageType);
+//     float damage = DamageCalculator.TrueDamageCalculator(strength, weaponWeight);
+//     float newArmourValue = DamageCalculator.ApDamage(damage, armourValue, armourDamageModifier);
+//     float remainingDamageAfterArmour = DamageCalculator.CalculatedRemainingDamage(damage, armourValue, armourDamageModifier);
+//     float newHealthValue =
+//         DamageCalculator.HpDamage(remainingDamageAfterArmour, heatlhValue, newArmourValue, healthDamageModifier);
+//     float remainingDamageAfterHealth = remainingDamageAfterArmour - heatlhValue;
+//     float attackSpeed = DamageCalculator.AttackSpeedCalculator(strength, weaponWeight, agilityValue);
+//     float damagePerSecond = DamageCalculator.DpsCalculator(attackSpeed, damage);
+//     
+//     Console.WriteLine($"Targeted Character Starting Health Points:  {heatlhValue}");
+//     Console.WriteLine($"Targeted Character Starting Armour Points:  {armourValue}");
+//     Console.WriteLine("");
+//     Console.WriteLine($"Attacking Character Weapon Weight:          {weaponWeight}");
+//     Console.WriteLine($"Attacking Character Strength:               {strength}");
+//     Console.WriteLine($"Attacking Character Weapon Damage Type:     {weaponDamageType.ToString()}");
+//     Console.WriteLine($"Attacking Character Calculated Damage:      {damage}");
+//     Console.WriteLine($"Attacking Character Agility:                {agilityValue}");
+//     Console.WriteLine($"Attacking Character Attack Speed:           {attackSpeed}");
+//     Console.WriteLine($"Attacking Character Damage Per Second:      {damagePerSecond}");
+//     Console.WriteLine("");
+//     Console.WriteLine($"Attacking Character Armour Damage Modifier: {armourDamageModifier}");
+//     Console.WriteLine($"Attacking Character Health Damage Modifier: {healthDamageModifier}");
+//     Console.WriteLine("");
+//     
+//     if (remainingDamageAfterArmour != damage)
+//     {
+//         Console.WriteLine($"Damage Remaining After Armour:              {remainingDamageAfterArmour}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Damage did not breach armour");
+//     }
+//     
+//     if (newHealthValue > 0)
+//     {
+//         Console.WriteLine("");
+//         Console.WriteLine($"Targeted Character Armour Points After Damage: {newArmourValue}");
+//         Console.WriteLine($"Targeted Character Health Points After Damage: {newHealthValue}");
+//         while (userInputString != "Exit")
+//         {
+//             Console.WriteLine("");
+//             Console.WriteLine("Would you like to deal damage again? (y/n)");
+//             userInputString = Console.ReadLine();
+//
+//             if (userInputString == "y")
+//             {
+//                 heatlhValue = newHealthValue;
+//                 armourValue = newArmourValue;
+//                 userInputString = "Exit";
+//             }
+//             else if (userInputString == "n")
+//             {
+//                 heatlhValue = 0;
+//                 userInputString = "Exit";
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Invalid Input");
+//             }
+//         }
+//         userInputString = string.Empty;
+//     }
+//     else
+//     {
+//         heatlhValue = 0;
+//         Console.WriteLine($"Targeted Character Armour Points After Damage:  {newArmourValue}");
+//         Console.WriteLine($"Targeted Character Health Points After Damage:  {newHealthValue}");
+//         Console.WriteLine($"Remaining Damage:                               {remainingDamageAfterHealth}");
+//         Console.WriteLine("");
+//     }
+//     
+// }
+// Console.WriteLine("Damage Calculator Stopped");
